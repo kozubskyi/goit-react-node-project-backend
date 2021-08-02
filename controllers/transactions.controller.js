30682b269ab3975057da64cae5703dbd539cba51
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-03 02:00:50 +0300

    написал код на добавление транзакций

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,4 +1,36 @@
-const { Router } = require("express");
-const router = Router();
+const { Router } = require("express")
+const router = Router()
 
-exports.transactionsController = router;
+const { authorize } = require("../middlewares/authorize")
+const { validate } = require("../middlewares/validate")
+const {
+  expenseTransactionSchema,
+  incomeTransactionSchema,
+  transactionIdSchema,
+} = require("../schemes/transactions.schemes")
+const { asyncWrapper } = require("../middlewares/async-wrapper")
+const { transactionsService } = require("../services/transactions.service")
+
+router.post(
+  "/expense",
+  authorize,
+  validate(expenseTransactionSchema),
+  asyncWrapper(async (req, res, next) => {
+    const updatedUser = await transactionsService.addExpenseTransaction(req.user, req.body)
+
+    res.status(201).json({ balance: updatedUser.balance, expenses: updatedUser.transactions.expenses })
+  })
+)
+
+router.post(
+  "/income",
+  authorize,
+  validate(incomeTransactionSchema),
+  asyncWrapper(async (req, res, next) => {
+    const updatedUser = await transactionsService.addIncomeTransaction(req.user, req.body)
+
+    res.status(201).json({ balance: updatedUser.balance, income: updatedUser.transactions.income })
+  })
+)
+
+exports.transactionsController = router
