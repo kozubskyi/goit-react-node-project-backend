2c5c01e76dedce74e1d1833b16b0cc35dab00e35
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-03 01:59:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.json())
 
 app.use("/api/v1/auth", authController)
 app.use("/api/v1/balance", balanceController)
-app.use("/api/v1/transaction", transactionsController)
+app.use("/api/v1/transactions", transactionsController)
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.message })
