090a42bd1d119d94ac987a9b55d6fb107b0d62e8
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-03 00:06:57 +0300

    Update balance.controller.js

--- controllers/balance.controller.js
+++ controllers/balance.controller.js
@@ -3,14 +3,16 @@ const router = Router()
 
 const { validate } = require("../middlewares/validate")
 const { balanceSchema } = require("../schemes/balance.schema")
+const { authorize } = require("../middlewares/authorize")
 const { asyncWrapper } = require("../middlewares/async-wrapper")
 const { balanceService } = require("../services/balance.service")
 
 router.post(
-  "/:userId",
+  "/",
+  authorize,
   validate(balanceSchema),
   asyncWrapper(async (req, res, next) => {
-    await balanceService.setBalance(req.body.balance, req.params.userId)
+    await balanceService.setBalance(req.body.balance, req.user._id)
 
     res.status(201).json({ message: "User balance has been updated" })
   })
