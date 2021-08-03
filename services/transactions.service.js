f3d1bf4e9ece061b2c3936667a652a21133e3bcf
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-03 11:54:37 +0300

    добавил логику на удаление транзакции и получения транзакций за месяц

--- services/transactions.service.js
+++ services/transactions.service.js
@@ -1,10 +1,11 @@
 const { UserModel } = require("../models/user.model")
 const uuid = require("uuid")
+const { BadRequest } = require("http-errors")
 
 class TransactionsService {
   async addExpenseTransaction(user, transaction) {
-    const transactionWithIdAndTime = { ...transaction, id: uuid.v4(), time: new Date(Date.now()) }
-    const updatedExpenses = [...user.transactions.expenses, transactionWithIdAndTime]
+    const transactionWithId = { ...transaction, id: uuid.v4() }
+    const updatedExpenses = [...user.transactions.expenses, transactionWithId]
 
     const updatedData = {
       transactions: { ...user.transactions, expenses: updatedExpenses },
@@ -17,8 +18,8 @@ class TransactionsService {
   }
 
   async addIncomeTransaction(user, transaction) {
-    const transactionWithIdAndTime = { ...transaction, id: uuid.v4(), time: new Date(Date.now()) }
-    const updatedIncome = [...user.transactions.income, transactionWithIdAndTime]
+    const transactionWithId = { ...transaction, id: uuid.v4() }
+    const updatedIncome = [...user.transactions.income, transactionWithId]
 
     const updatedData = {
       transactions: { ...user.transactions, income: updatedIncome },
@@ -28,6 +29,55 @@ class TransactionsService {
     const updatedUser = await UserModel.findByIdAndUpdate(user._id, updatedData, { new: true })
 
     return updatedUser
+  }
+
+  async deleteTransaction(user, transactionId) {
+    const incomeTransaction = user.transactions.income.find((transaction) => transaction.id === transactionId)
+
+    if (!incomeTransaction) {
+      const expenseTransaction = user.transactions.expenses.find((transaction) => transaction.id === transactionId)
+
+      if (!expenseTransaction) throw new BadRequest(`Transaction with id '${transactionId}' was not found`)
+
+      const updatedData = {
+        balance: user.balance + expenseTransaction.sum,
+        transactions: {
+          income: user.transactions.income,
+          expenses: user.transactions.expenses.filter((transaction) => transaction.id !== transactionId),
+        },
+      }
+
+      const updatedUser = await UserModel.findByIdAndUpdate(user._id, updatedData, { new: true })
+
+      return updatedUser
+    }
+
+    const updatedData = {
+      balance: user.balance - incomeTransaction.sum,
+      transactions: {
+        income: user.transactions.income.filter((transaction) => transaction.id !== transactionId),
+        expenses: user.transactions.expenses,
+      },
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(user._id, updatedData, { new: true })
+
+    return updatedUser
+  }
+
+  async getTransactionsByMonth(transactions, month) {
+    const filteredTransactions = transactions.filter((transaction) => {
+      const arr = transaction.date.split("-")
+      const transactionMonth = `${arr[1]}-${arr[0]}`
+
+      console.log(transactionMonth, month)
+
+      return transactionMonth === month
+    })
+
+    if (filteredTransactions.length === 0) throw new BadRequest("No transactions in this month or bad request")
+
+    return filteredTransactions
   }
 }
 
