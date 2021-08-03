f9a4ac0bf8c4be14798d1f4bbff458f48d5db947
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-03 23:42:46 +0300

    Update transactions.schemes.js

--- schemes/transactions.schemes.js
+++ schemes/transactions.schemes.js
@@ -1,35 +1,39 @@
 const Joi = require("joi").extend(require("@joi/date"))
 
+const expenseCategories = [
+  "products",
+  "alcohol",
+  "entertainment",
+  "health",
+  "transport",
+  "allForHome",
+  "technics",
+  "communalAndConnection",
+  "sportAndHobbies",
+  "education",
+  "others",
+]
+
+const incomeCategories = ["salary", "others"]
+
 exports.expenseTransactionSchema = Joi.object({
   type: Joi.string().valid("expense").required(),
   category: Joi.string()
-    .valid(
-      "products",
-      "alcohol",
-      "entertainment",
-      "health",
-      "transport",
-      "allForHome",
-      "technics",
-      "communalAndConnection",
-      "sportAndHobbies",
-      "education",
-      "others"
-    )
+    .valid(...expenseCategories)
     .required(),
   sum: Joi.number().required(),
   description: Joi.string().required(),
   date: Joi.date().format("DD.MM.YYYY").required(),
-  // owner: Joi.required(),
 })
 
 exports.incomeTransactionSchema = Joi.object({
   type: Joi.string().valid("income").required(),
-  category: Joi.string().valid("salary", "others").required(),
+  category: Joi.string()
+    .valid(...incomeCategories)
+    .required(),
   sum: Joi.number().required(),
   description: Joi.string().required(),
   date: Joi.date().format("DD.MM.YYYY").required(),
-  // owner: Joi.required(),
 })
 
 exports.monthSchema = Joi.object({
