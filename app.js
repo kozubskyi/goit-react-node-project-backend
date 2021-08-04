324164c28f644ef8c8be87a6720d413c1d46c67d
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-04 09:16:34 +0300

    переименовал balance service/controller в user service/controller

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 const logger = require("morgan")
 
 const { authController } = require("./controllers/auth.controller")
-const { balanceController } = require("./controllers/balance.controller")
+const { userController } = require("./controllers/user.controller")
 const { transactionsController } = require("./controllers/transactions.controller")
 
 const app = express()
@@ -36,7 +36,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/auth", authController)
-app.use("/api/v1/balance", balanceController)
+app.use("/api/v1/user", userController)
 app.use("/api/v1/transactions", transactionsController)
 
 app.use((err, req, res, next) => {
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,17 +5,17 @@ const { authorize } = require("../middlewares/authorize")
 const { validate } = require("../middlewares/validate")
 const { balanceSchema } = require("../schemes/balance.schema")
 const { asyncWrapper } = require("../middlewares/async-wrapper")
-const { balanceService } = require("../services/balance.service")
+const { userService } = require("../services/user.service")
 
 router.post(
-  "/",
+  "/balance",
   authorize,
   validate(balanceSchema),
   asyncWrapper(async (req, res, next) => {
-    const balance = await balanceService.setBalance(req.user._id, req.body.balance)
+    const balance = await userService.setBalance(req.user._id, req.body.balance)
 
     res.status(200).json({ balance })
   })
 )
 
-exports.balanceController = router
+exports.userController = router
