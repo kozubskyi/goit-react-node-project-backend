321af886f60c8c63a5b728babfe6a71e71c6cc1e
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-07 23:55:28 +0300

    Delete auth.serializer.js

--- serializers/auth.serializer.js
+++
@@ -1,9 +0,0 @@
-function prepareUserWithToken(userWithToken) {
-  return {
-    id: userWithToken.user._id,
-    email: userWithToken.user.email,
-    token: userWithToken.token
-  };
-}
-
-exports.prepareUserWithToken = prepareUserWithToken;
