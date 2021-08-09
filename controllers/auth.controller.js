2e2414e9c69cd14578e42a0131d29b9204e0ee42
Anastasiia Lozovych <anna.collana@gmail.com>
2021-08-09 01:57:20 +0100

    Adding swagger-ui

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,68 +1,49 @@
-const { Router } = require("express")
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-const { asyncWrapper, validate, authorize } = require("../middlewares/middlewares")
-const { userSignUpSchema, userSignInSchema } = require("../validation/schemes/auth.schema")
-const { authService } = require("../services/auth.service")
-const { prepareUser, prepareUserWithToken } = require("../serializers")
-
-/**
- * @swagger
- * components:
- *  schemas:
- *   User:
- *   type:object
- *     required:
- *      properties:
- *       id:
- *        type:string
- *        description:The auto-generated id of the user
- *         email:
- *          type:string
- *          description:The email of the user
- *        example:
- *         id:610a804086a24935c94d0ffa
- *         email:anastasiia@gmail.com
- */
+const { asyncWrapper, validate, authorize } = require("../middlewares/middlewares");
+const { userSignUpSchema, userSignInSchema } = require("../validation/schemes/auth.schema");
+const { authService } = require("../services/auth.service");
+const { prepareUser, prepareUserWithToken } = require("../serializers");
 
 router.post(
   "/signup",
   validate(userSignUpSchema),
   asyncWrapper(async (req, res, _) => {
-    const user = await authService.signUp(req.body)
+    const user = await authService.signUp(req.body);
 
     const response = {
       message: "User has been created",
-      user: prepareUser(user),
-    }
+      user: prepareUser(user)
+    };
 
-    res.status(201).json(response)
+    res.status(201).json(response);
   })
-)
+);
 
 router.post(
   "/signin",
   validate(userSignInSchema),
   asyncWrapper(async (req, res, _) => {
-    const userWithToken = await authService.signIn(req.body)
+    const userWithToken = await authService.signIn(req.body);
 
     const response = {
       message: "User has been signed in",
-      user: prepareUserWithToken(userWithToken),
-    }
+      user: prepareUserWithToken(userWithToken)
+    };
 
-    res.status(200).json(response)
+    res.status(200).json(response);
   })
-)
+);
 
 router.post(
   "/signout",
   authorize,
   asyncWrapper(async (req, res, _) => {
-    await authService.signOut(req.user)
+    await authService.signOut(req.user);
 
-    res.status(204).send()
+    res.status(204).send();
   })
-)
+);
 
-exports.authController = router
+exports.authController = router;
--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,20 +1,20 @@
-const { Router } = require("express")
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-const { asyncWrapper, validate, authorize, createSchema } = require("../middlewares/middlewares")
-const { categoriesService } = require("../services/services")
-const { typeSchema } = require("../validation/schemes/transactions.schemes")
+const { asyncWrapper, validate, authorize, createSchema } = require("../middlewares/middlewares");
+const { categoriesService } = require("../services/services");
+const { typeSchema } = require("../validation/schemes/transactions.schemes");
 
 router.get(
   "/:type",
   authorize,
   validate(typeSchema, "params"),
   asyncWrapper(async (req, res, _) => {
-    const categories = await categoriesService.getCategories(req.params.type)
+    const categories = await categoriesService.getCategories(req.params.type);
 
-    res.status(200).json({ categories })
+    res.status(200).json({ categories });
   })
-)
+);
 
 router.post(
   "/:type",
@@ -22,26 +22,26 @@ router.post(
   validate(typeSchema, "params"),
   validate(createSchema("name")),
   asyncWrapper(async (req, res, _) => {
-    const { name, type } = await categoriesService.addCategory(req.user, req.body.name, req.params.type)
+    const { name, type } = await categoriesService.addCategory(req.user, req.body.name, req.params.type);
 
     const response = {
       message: "Category has been created",
-      category: { name, type },
-    }
+      category: { name, type }
+    };
 
-    res.status(201).json(response)
+    res.status(201).json(response);
   })
-)
+);
 
 router.delete(
   "/:category",
   authorize,
   validate(createSchema("category"), "params"),
   asyncWrapper(async (req, res, _) => {
-    await categoriesService.deleteCategory(req.user, req.params.category)
+    await categoriesService.deleteCategory(req.user, req.params.category);
 
-    res.status(200).json({ message: "Category has been deleted" })
+    res.status(200).json({ message: "Category has been deleted" });
   })
-)
+);
 
-exports.categoriesController = router
+exports.categoriesController = router;
