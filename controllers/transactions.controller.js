7f81d28e49ea0af2f745569f71b3af40edd81c44
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-11 18:31:48 +0300

    добавил получение всех транзакций пользователя по типу

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -3,7 +3,7 @@ const router = Router()
 
 const { asyncWrapper, validate, authorize, createSchema } = require("../middlewares/middlewares")
 const { validateTransaction } = require("../validation/validate-transaction")
-const { summarySchema, periodSchema } = require("../validation/schemes/transactions.schemes")
+const { typeSchema, summarySchema, periodSchema } = require("../validation/schemes/transactions.schemes")
 const { transactionsService } = require("../services/transactions.service")
 
 router.post(
@@ -30,6 +30,17 @@ router.delete(
     await transactionsService.deleteTransaction(req.user._id, req.params.id)
 
     res.status(200).json({ message: "Transaction has been deleted" })
+  })
+)
+
+router.get(
+  "/:type",
+  authorize,
+  validate(typeSchema, "params"),
+  asyncWrapper(async (req, res, _) => {
+    const transactions = await transactionsService.getTransactions(req.user._id, req.params.type)
+
+    res.status(200).json(transactions)
   })
 )
 
